b5beee98d6fb27bb6bf7b96adef774d85eb3575b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-30 03:26:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    binding.pry
+    @user = User.new(name: params["name"], email: params["email"], params["password"])
+    @user.save
     redirect '/users/home'
   end
 
