08302c1ab03b9496a192cb43267ac46a12b5b470
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-30 04:00:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    binding.pry
+    @user = User.find_by(email: params["email"], password: params["password"])
     redirect '/users/home'
   end
 
